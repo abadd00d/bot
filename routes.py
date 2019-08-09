c08b8bfcf9e5c1e1f7bb11d486470ed6c96b5da6
Vladimir Zaytsev <v.zaytsev@ngenix.net>
2019-08-09 14:56:38 +0300

    [fix]

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from views import get_stats, receive_message_from_user, send_message_to_user
 
-def setup_routes(app, bot):
+def setup_routes(app):
     app.router.add_get(app['config']['server']['routes']['stats'], get_stats)
     app.router.add_post(app['config']['server']['routes']['in'], receive_message_from_user)
     app.router.add_post(app['config']['server']['routes']['out'], send_message_to_user)
